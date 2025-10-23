a38dca2d38eab53c07d3225670e258a70a21787c
ArsuPlayer <mauricioflores@liceovvh.cl>
2025-10-23 15:07:08 -0300

    avance

--- 07_bucles_while_for/static/script.js
+++ 07_bucles_while_for/static/script.js
@@ -42,3 +42,50 @@ function ejercicio1() {
 
 
 }
+
+//2️⃣ Bucle while: Cuenta regresiva del n al 1 (n es ingresado por teclado) Crea un programa que muestre una cuenta regresiva desde n hasta 1 utilizando un bucle while.
+
+function ejercicio2() {
+
+    let contador = parseInt(prompt("Ingresar Numero"));
+    let resultado = "contamos:"
+    while (contador > 0) {
+        resultado += ` ${contador}`;
+        contador--;
+    }
+    alert(resultado)
+}
+
+//3️⃣ Bucle while: Sumar los n primeros números Usando un bucle for, calcula la suma de los números n primeros números  muestra el resultado en la consola.
+
+function ejercicio3() {
+
+    let suma = 0;
+    let numero = parseInt(prompt("Ingresar un numero: "))
+    let contador = 1;
+    let resultado = "Suma: 0";
+    while (contador <= numero) {
+        suma += contador;
+        resultado += `  + ${contador}`;
+        contador++
+    }
+    alert(` ${resultado} = ${suma} `);
+}
+
+//4️⃣ Bucle while: Imprimir múltiplos de 2 del 1 al n (n es el límite de números a encontrar) Escribe un programa que use un bucle for para imprimir los múltiplos de 2 que hay entre 1 y 10 (inclusive).
+
+function ejercicio4() {
+
+    let numero = parseInt(prompt("Ingresa número: "))
+    let count = 1;
+    let resultado = " El resultado es: "
+    while (count <= numero) {
+        count++
+
+        if (count %2 == 0) {
+            resultado += `${count}`
+        }
+
+    }
+    alert(` ${resultado}`)
+}
